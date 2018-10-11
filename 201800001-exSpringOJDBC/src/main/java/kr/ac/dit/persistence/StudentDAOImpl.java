7e363dc821f92590256c97059b842ce52ae9bef4
D7701 <D7701@DESKTOP-ODBNK9S>
2018-10-11 15:27:37 +0900

    컨트롤러

---
+++ 201800001-exSpringOJDBC/src/main/java/kr/ac/dit/persistence/StudentDAOImpl.java
@@ -0,0 +1,54 @@
+package kr.ac.dit.persistence;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+import kr.ac.dit.domain.StudentVO;
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+	@Override
+	public void insertStudent(StudentVO studentVO) throws Exception {
+		String no = studentVO.getNo();
+
+		String name = studentVO.getName();
+
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+
+		Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "201412395", "201412395");
+
+		PreparedStatement pstmt = conn.prepareStatement("insert into student values(?,?)");
+
+		pstmt.setString(1, no);
+
+		pstmt.setString(2, name);
+
+		pstmt.executeUpdate();
+
+		pstmt.close();
+
+		conn.close();
+
+		}
+	@Override
+	public List<StudentVO> selectStudentList() throws Exception {
+		List<StudentVO> items = new ArrayList<StudentVO>();
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "201412395", "201412395");
+		PreparedStatement pstmt = conn.prepareStatement("select * from student");
+		ResultSet rs = pstmt.executeQuery();
+		while(rs.next()) {
+			StudentVO item = new StudentVO();
+			item.setNo(rs.getString("no"));
+			item.setName(rs.getString("name"));
+			items.add(item);
+		}
+		pstmt.close();
+		rs.close();
+		conn.close();
+		return items;
+	}
+}
